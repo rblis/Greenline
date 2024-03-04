3df6677b6bec7cd5ccde8992696a97b896a2b3ab
Ryan Biswas <rblis@outlook.com>
2024-03-03 22:15:05 -0500

    Main component for holding all frontpage components

--- frontend/src/modules/MainStockDashboard/components/DashboardContainer/DashboardContainer.tsx
+++ frontend/src/modules/MainStockDashboard/components/DashboardContainer/DashboardContainer.tsx
@@ -3,6 +3,7 @@ import './DashboardContainerStyles.css'
 import CardTable from "../../../Common/components/CardTable/CardTable";
 import axios from "axios";
 import SearchBar from "../../../Common/components/SearchBar/SearchBar";
+import StockDetailPane from "../StockDetailPane/StockDetailPane";
 interface Props {
 }
 
@@ -10,6 +11,10 @@ function DashboardContainer({}: Props) {
     const [columns, setColumns] = useState();
     const [rawData, setRawData] = useState();
     const [loading, setLoading] = useState(true);
+    const [overviewData, setOverviewData] = useState([{'test': 'data'}]);
+    const [query, setQuery] = useState('');
+    const [dropdownItems, setDropdownItems] = useState([]);
+    
     useEffect(() => {
         axios
             .get(
@@ -22,30 +27,52 @@ function DashboardContainer({}: Props) {
                 }));
                 setRawData(response.data.rawData);
                 setLoading(false);
+                console.log(response);
             }).catch(error => console.log(error));
     }, []);
     
     async function search() {
+        if (query.length > 0) {
+            axios.get(
+                'http://localhost:5057/dashboard/search/' + query,
+                {headers: {'Access-Control-Allow-Origin' : 'Allow'}}
+            ).then(response => {
+                console.log(response.data.body);
+                setDropdownItems(response.data.body);
+            }).catch(error => console.log(error));
+        }
+    }
+    
+    const fetchStockOverview = async (symbol: string) => {
         axios.get(
-            'http://localhost:5057/dashboard/search/' + query,
+            'http://localhost:5057/dashboard/overview/' + symbol,
             {headers: {'Access-Control-Allow-Origin' : 'Allow'}}
         ).then(response => {
             console.log(response.data.body);
-            var decoratedResults = response.data.body.map( (item: any) => ( item.symbol + ' ' + item.name));
-            setDropdown(decoratedResults);
+            let data = [
+                {'Ask': response.data.body[0].ask},
+                {'Bid': response.data.body[0].bid},
+                {'Name': response.data.body.displayName},
+                {'EPS Currrent Year': response.data.body[0].epsCurrentYear},
+                {'EPS Trailing 12 Months': response.data.body[0].epsTrailingTwelveMonths},
+                {'50 Day Average': response.data.body[0].fiftyDayAverage},
+                {'50 Day Average Change %': response.data.body[0].fiftyDayAverageChangePercent},
+                {'52-Week Range': response.data.body[0].fiftyTwoWeekRange},
+                {'Market Cap': response.data.body[0].marketCap}, 
+            ];
+            // @ts-ignore
+            setOverviewData(data);
         }).catch(error => console.log(error));
     }
-    
-    const [query, setQuery] = useState('tsla');
-    const [dropdown, setDropdown] = useState([]);
 
     return (
         <div className={'dashboard_window flex_col'}>
             <h1 className={'center font1'}>Dashboard</h1>
-            <SearchBar setQuery={setQuery} search={search} dropdown={dropdown}/>
-            <div className={'stock_lists flex_row'}>
-                {loading ? '' : <CardTable columns={columns} rawData={rawData}/>}
+            <SearchBar setQuery={setQuery} search={search} dropdownItems={dropdownItems}/>
+            <div className={'round_borders flex_row'}>
+                {loading ? '' : <CardTable columns={columns} rawData={rawData} clickHandler={fetchStockOverview}/>}
             </div>
+            <StockDetailPane data={overviewData}/>
         </div>
     );
 }
