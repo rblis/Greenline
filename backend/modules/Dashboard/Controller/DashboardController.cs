7f28315f6f45dfcadece3e977748c37ab84fa5c2
Ryan Biswas <rblis@outlook.com>
2024-03-03 22:29:21 -0500

    new backend api endpoints

--- backend/modules/Dashboard/Controller/DashboardController.cs
+++ backend/modules/Dashboard/Controller/DashboardController.cs
@@ -17,7 +17,7 @@ public class DashboardController : ControllerBase
     }
     
     [HttpGet("portfolio")]
-    public IActionResult getDashboard()
+    public IActionResult fetchPortfolio()
     {
         var stocks = db.Stocks.ToList().Select(s => s.hydrateStock());
         var columns = typeof(Stock.Model.Schema.Stock).GetProperties().Select(pp =>
@@ -38,7 +38,7 @@ public class DashboardController : ControllerBase
     }
 
     [HttpGet("search/{query}")]
-    public IActionResult getSearchResults([FromRoute] string query)
+    public IActionResult fetchSearchResults([FromRoute] string query)
     {
         System.Diagnostics.Debug.WriteLine($"Received search request for {query}");
         using (var client = new HttpClient())
@@ -47,6 +47,29 @@ public class DashboardController : ControllerBase
             {
                 Method = HttpMethod.Get,
                 RequestUri = new Uri($"https://mboum-finance.p.rapidapi.com/v1/markets/search?search={query}"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "19eaceee83msh17a42ce61ffed43p135b50jsnb69e2cdbe427" },
+                    { "X-RapidAPI-Host", "mboum-finance.p.rapidapi.com" },
+                },
+            };
+            var response = client.SendAsync(request).Result;
+            response.EnsureSuccessStatusCode();
+            var json = response.Content.ReadAsStringAsync().Result;
+            return Ok(json);
+        }
+    }
+    
+    [HttpGet("overview/{query}")]
+    public IActionResult fetchStockOverview([FromRoute] string query)
+    {
+        System.Diagnostics.Debug.WriteLine($"Received search request for {query}");
+        using (var client = new HttpClient())
+        {
+            var request = new HttpRequestMessage()
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://mboum-finance.p.rapidapi.com/v1/markets/stock/quotes?symbol={query}"),
                 Headers =
                 {
                     { "X-RapidAPI-Key", "19eaceee83msh17a42ce61ffed43p135b50jsnb69e2cdbe427" },
